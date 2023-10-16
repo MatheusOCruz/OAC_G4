a29ac076cd68acd53e1a27c92a9d939574954065
Matheus Cruz <95373076+MatheusOCruz@users.noreply.github.com>
2023-10-16 20:25:57 -0300

    Add files via upload

--- modules/input.s
+++ modules/input.s
@@ -29,8 +29,6 @@ NO_INPUT:
 	ret
 
 MV_UP:	
-	#addi sp,sp,4
-
 	la t0,link_pos
 	la t3,tile_map_mundo_aberto
 	addi t3,t3,8 #pegando a matriz da dela e pulando os dados
@@ -38,21 +36,20 @@ MV_UP:
 	lh t1,2(t0) # eixo y
 	lh t2,0(t0) #eixo x
 	
-	srli t5,t1,4	#indï¿½ce na matriz em y
-	srli t6,t2,4	#indï¿½ce na matriz em x
+	srli t5,t1,4	#indíce na matriz em y
+	srli t2,t2,4	#indíce na matriz em x
 	addi t5,t5,-5 
-	li t4,20        #calculando a posiï¿½ï¿½o do quadrado de cima do boneco
-	mul t5,t5,t4    #cada linha da matriz tem 20 valores, entao multiplica pos y por 20
+	li t4,20      #calculando a posição do quadrado de cima do boneco
+	mul t6,t6,t4
 	add t3,t3,t6
-	add t3,t3,t5    #t3 = posiÃ§Ã£o na matriz
-	div t5,t5,t4    #volta t5 para o valor do indice
+	add t3,t3,t2
 	
 	li t4,-1
 	beq t5,t4,TELA_UP   # t5 = -1 e pq passou do limite da tela, troca de mapa 
 	
 	lb t3,(t3)	     #pega o valor do quadrado
 	#beq t3,(valor do preto), cavernosa
-	bne t3,zero,COLIDE #se o quadrado nï¿½o for vazio, testa colisao
+	bne t3,zero,NO_INPUT #se o quadrado não for vazio, ignora o input
 	
 	addi t1,t1,-16 
 	sh t1,2(t0)
@@ -84,23 +81,23 @@ MV_LEFT:
 	la t3,tile_map_mundo_aberto
 	addi t3,t3,8 #pegando a matriz da dela e pulando os dados
 	
-	lh t1,2(t0) #eixo y
+	lh t1,2(t0) # eixo y
 	lh t2,0(t0) #eixo x
 	
-	srli t5,t1,4	#indï¿½ce na matriz em y
-	srli t6,t2,4	#indï¿½ce na matriz em x
-	addi t5,t5,-4
-	li t4,20        #calculando a posiï¿½ï¿½o do quadrado da esquerda do boneco
-	mul t5,t5,t4
+	srli t1,t1,4	#indíce na matriz em y
+	srli t5,t2,4	#indíce na matriz em x
+	addi t1,t1,-4
+	li t4,20      #calculando a posição do quadrado da esquerda do boneco
+	mul t1,t1,t4
+	add t3,t3,t1
 	add t3,t3,t5
-	add t3,t3,t6
 	addi t3,t3,-1
-	div t5,t5,t4
 	
-	beq t6,zero,TELA_LEFT
+	
+	beq t5,zero,TELA_LEFT
 	
 	lb t3,(t3)	     #pega o valor do quadrado
-	bne t3,zero,COLIDE #se o quadrado nï¿½o for vazio, testa colisÃ£o
+	bne t3,zero,NO_INPUT #se o quadrado não for vazio, ignora o input
 	
 	addi t2,t2,-16
 	sh t2,0(t0)
@@ -134,20 +131,19 @@ MV_DOWN:
 	lh t1,2(t0) # eixo Y
 	lh t2,0(t0) #eixo x
 	
-	srli t5,t1,4	#indï¿½ce na matriz em y
-	srli t6,t2,4	#indï¿½ce na matriz em x
+	srli t5,t1,4	#indíce na matriz em y
+	srli t2,t2,4	#indíce na matriz em x
 	addi t5,t5,-3
-	li t4,20      #calculando a posiï¿½ï¿½o do quadrado de baixo do boneco
-	mul t5,t5,t4
+	li t4,20      #calculando a posição do quadrado de baixo do boneco
+	mul t6,t6,t4
 	add t3,t3,t6
-	add t3,t3,t5
-	div t5,t5,t4
+	add t3,t3,t2
 	
 	li t4, 11
 	beq t4,t5,TELA_DOWN
 	
 	lb t3,(t3)	     #pega o valor do quadrado
-	bne t3,zero,COLIDE #se o quadrado nï¿½o for vazio, testa colisao
+	bne t3,zero,NO_INPUT #se o quadrado não for vazio, ignora o input
 	
 	addi t1,t1,16
 	sh t1,2(t0)
@@ -180,22 +176,21 @@ MV_RIGHT:
 	lh t1,2(t0) # eixo Y
 	lh t2,0(t0) #eixo x
 	
-	srli t5,t1,4    #indï¿½ce na matriz em y
-	srli t6,t2,4	#indï¿½ce na matriz em x
-	addi t5,t5,-4
+	srli t1,t1,4    #indíce na matriz em y
+	srli t5,t2,4	#indíce na matriz em x
+	addi t1,t1,-4
 	li t4,20      
-	mul t5,t5,t4
-	add t3,t3,t6
+	mul t1,t1,t4
+	add t3,t3,t1
 	add t3,t3,t5
-	addi t3,t3,1  #calculando a posiï¿½ï¿½o do quadrado da direita do boneco
-	div t5,t5,t4
+	addi t3,t3,1  #calculando a posição do quadrado da direita do boneco
 	
 	
 	li t4,19
-	beq t6,t4,TELA_RIGHT
+	beq t5,t4,TELA_RIGHT
 	
 	lb t3,(t3)	     #pega o valor do quadrado
-	bne t3,zero,COLIDE #se o quadrado nï¿½o for vazio, testa colisao
+	bne t3,zero,NO_INPUT #se o quadrado não for vazio, ignora o input
 	
 	addi t2,t2,16
 	sh t2,0(t0)
@@ -227,5 +222,4 @@ ANIM_2:
 	sb t2,0(t0)
 	ret
 	
-COLIDE: #por enquanto so cancela o movimento. depois da pra adicionar outros testes de colisao
-	ret
+	
--- modules/map_manager.s
+++ modules/map_manager.s
@@ -1,7 +1,8 @@
 
 .data
 
-.include "../assets/tiles/tilemap_mundo_aberto.data"
+.include "tiles/tilemap_mundo_aberto.data"
+.include "tiles/teste_mapa_tilemap.data"
 x_inicial: .byte 0
 y_inicial: .byte 64
 
@@ -23,31 +24,54 @@ MAP_MANAGER:
 	add s3,sp,zero
 	 # salva oq tinha em s0 na pilha 
 	mv a3,s0 # bota o frame em a3 antes de botar s0 na pilha
-	addi sp,sp,-8
-	sw s0,0(sp)
-	sw ra,4(sp)
-	la s0, tile_map_mundo_aberto # s0 tem o endereco do tile map (na teoria pq ta dando ruim)
-	li a1,0
-	mv s6,zero
-	li a2,64
-	addi s0,s0,8
+	addi sp,sp,-16
+	sw ra,0(sp)
+	sw s0,4(sp)
+	sw s1,8(sp)
+	sw s2,12(sp)
+	
+	la s0, teste_mapa_tilemap # s0 tem o endereco do tile map (na teoria pq ta dando ruim)
+	lw s1, 0(s0)   # largura do mapa inteiro, necessario para pular mudar y do mapa
+	
+	addi s0,s0,8   # atualiza o endereco do s0 pro primeiro que contem info 
+	
+	
+	la t1, map_localtion # var com a posicao da tela atual 
+	lb t2,0(t1)  # x da matriz
+	lb t3,1(t1)  # y da matriz
+	
+	li t4,20      # largura fixa dos mapas
+	mul t2,t2,t4  # como a largura do tileset e sempre fixa (a largura da tela) n precisamos ler do arquivo
+	add s0,s0,t2  # adiciona ao x no endereco inical do tilemap
+	
+	li t2,11      # t2 ja foi usado, da pra usar aqui, altura do mapa tmb e fixa = 11 
+	mul t3,t3,t2  # quantidade de linhas a ser pulada para cheagar no tile inicial
+	
+	mul t3,t3,s1  # multiplica a quantidade de linhas pela quantidade de elementos na linha
+	add s0,s0,t3  # na teoria com fe s0 tem o inicial
+	
+	
+	li a1,0	     # x inicial do print
+	li a2,64     # y inicial do print
+	mv s2,zero   # contador de tiles por linha (max = 20)
 	
 PRINT_MAP_LOOP:
 	
-	la a0, tiles_mundo_aberto
+	la a0, game_tiles
 	# a1 ja e definido
 	# a2 ja e definido
 	# a3 e fixo
 	li a4,16 # tamanho do trem
 	lb a6,0(s0) # o tile q vai printar
-	la t0, map_localtion
-	lh a7, 0(a0)
+	
 	call PRINT_SPRITE
 	
   	
 	addi s0,s0,1
-	addi s6,s6,1
-			# apos printar o tile
+	addi s2,s2,1
+	li t0, 20
+	beq s2,t0,PRINT_MAP_NL
+PRINT_MAP_P2:				
 	addi a1,a1,16
 	li t0,320
 	bne a1,t0,PRINT_MAP_LOOP
@@ -62,13 +86,50 @@ PRINT_MAP_LOOP:
 
 RETURN_FROM_PRINT_MAP:
 	#tira os valores da pilha, e volta para o game_loop
-	addi s4,zero,0xff
-	lw s0, 0(sp)
-	lw ra, 4(sp)
-	addi sp,sp,8
+	lw ra,0(sp)
+	lw s0,4(sp)
+	lw s1,8(sp)
+	lw s2,12(sp)
+	addi sp,sp,16
 	
 	ret
 	
 	
+PRINT_MAP_NL:
 	
+	add s0,s0,s1
+	sub s0,s0,s2  # mesma logica do print tile pra achar primeiro da proxima linha
+	mv s2,zero    # reseta contador da linha
+	j PRINT_MAP_P2
+	
+
+	
+# a0 -> 0 se for 
+	
+	
+MAP_TRANSITION_ANIMATION:
+
+
+	ret
+	
+	
+
+CAVE_MANAGER:
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+				
 	
