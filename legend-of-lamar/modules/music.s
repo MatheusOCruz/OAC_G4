a56579c5b050d3155d5bc7a2d815e82ce11e3ceb
Pedro Santos <pedrosantosdevelop@gmail.com>
2023-12-16 19:01:24 -0300

    Conserta a musica dando ruim

--- legend-of-lamar/modules/music.s
+++ legend-of-lamar/modules/music.s
@@ -15,49 +15,47 @@ CURRENT_NOTE_DURATION: .word 300 # A duracao da nota atual para ser usada na mai
 
 .text
 
-
-
-
 MUSIC_MANAGER:
 	la t0, CURRENT_NOTE_DURATION		# So toca uma nota nova passados o tempo da ultima
 	lw t0, 0(t0)						# Carrega o valor da duração da nota
 	csrr t1, time 						# Carrega o tempo atual
 	sub t1, t1, s9 						# Subtrai o tempo atual do tempo da ultima nota
-	ble t1, t0, MUSIC_RET				# Não toca se n passou o tempo ms
+	ble t1, t0, MUSIC_RET				# Não toca se n passou o tempo da nota
 	b MUSIC_PLAY					# Toca a nota				
 
 
 # Play
 MUSIC_PLAY:
-	la t0, NOTES		      	 		# Carrega o endereco inicial das notas
+	la t0, NOTES		      	 	# Carrega o endereco inicial das notas
 	la t1, CURRENT_NOTE_INDEX  		# Carrega o indice da nota atual
 	la t3, CURRENT_NOTE_DURATION 	# Carrega o endereco da duracao da nota
 	lw t2, 0(t1) 					# Carrega o valor do indice
 	add t0, t0, t2 					# Adiciona o valor do indice no endereco das notas
 	
-	lw a0, 0(t0)  	# Carrega a nota atual
-	lw a1, 4(t0)  	# Carrega a duracao
-	li a2 0 		# Define o intrumento MIDI
-	li a3 110 		# Define o volume
-	li a7 31 		# Define a chamada para o MIDI
-	ecall			# toca a nota
+	lw a0, 0(t0)  					# Carrega a nota atual
+	lw a1, 4(t0)  					# Carrega a duracao
+	li a2 0 						# Define o intrumento MIDI
+	li a3 110 						# Define o volume
+	li a7 31 						# Define a chamada para o MIDI
+	ecall							# toca a nota
 	
-	addi t2, t2, 8 	# Incrementar o valor do index
-	sw t2, 0(t1) 	# Guardar proximo index no CURRENT_NOTE_INDEX
-	sw a1, 0(t3)      # Guarda a dura��o da nota no CURRENT_NOTE_DURATION
+	addi t2, t2, 8 					# Incrementar o valor do index
+	sw t2, 0(t1) 					# Guardar proximo index no CURRENT_NOTE_INDEX
+	sw a1, 0(t3)      				# Guarda a dura��o da nota no CURRENT_NOTE_DURATION
 	
 	# Se chegamos no indice final reseta a musica
-	la t3, NUM_OF_NOTES 		# Carrega o endereco do NUM_OF_NOTES
+	la t3, NUM_OF_NOTES 			# Carrega o endereco do NUM_OF_NOTES
 	lw t3, 0(t3)		    		# Substitui o t3 pelo valor no endereco
 	li t5, 8			    		# Carrega 8 	    	       
-	ble  t2, t4, FIM_MUSIC		    	       	# Se o indice atual for maior do que o NUM_OF_NOTES agt zera o indice
+	ble  t2, t4, FIM_MUSIC		   	# Se o indice atual for maior do que o NUM_OF_NOTES agt zera o indice
 	
 	la t0, CURRENT_NOTE_INDEX 		# Carrega o endereco do index
 	la t1, CURRENT_NOTE_DURATION 	# Carrega o endereco da nota atual
-	la t2, NOTES 			# Carrega o endereço das 
-	lw t3, 0(t2)			# Carrega a duração da primeira nota
-	sw zero, 0(t0)			      		# Zera o valor
-	sw t3, 0(t1)			      		# Carrega a duração da primeira nota
+	la t2, NOTES 					# Carrega o endereço das 
+	lw t3, 0(t2)					# Carrega a duração da primeira nota
+	sw zero, 0(t0)			      	# Zera o valor
+	sw t3, 4(t1)			      	# Carrega a duração da primeira nota
+	
 FIM_MUSIC:
 	csrr s9, time	
 
